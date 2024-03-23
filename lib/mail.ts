c57f5f9193157bb03ca69990e512f6cea944c347
Karanraj Mehta <karanrajmehta06@gmail.com>
2024-03-23 15:35:56 +0530

    feat: nodemailer for email sending

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,12 +1,21 @@
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from 'nodemailer';
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_USER,
+    pass: process.env.AUTH_PASS,
+  },
+});
+
 export async function sendTwoFactorTokenEmail(email: string, token: string) {
-  await resend.emails.send({
-    from: 'onboarding@karanraj.me',
+  await transporter.sendMail({
+    from: process.env.AUTH_USER,
     to: email,
     subject: '2FA Code',
     html: `<p>Your 2FA code: ${token}</p>`,
@@ -16,8 +25,8 @@ export async function sendTwoFactorTokenEmail(email: string, token: string) {
 export async function sendPasswordResetEmail(email: string, token: string) {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: 'onboarding@karanraj.me',
+  await transporter.sendMail({
+    from: process.env.AUTH_USER,
     to: email,
     subject: 'Reset your password',
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
@@ -27,8 +36,8 @@ export async function sendPasswordResetEmail(email: string, token: string) {
 export async function sendVerificationEmail(email: string, token: string) {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-  await resend.emails.send({
-    from: 'onboarding@karanraj.me',
+  await transporter.sendMail({
+    from: process.env.AUTH_USER,
     to: email,
     subject: 'Confirm your email',
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
